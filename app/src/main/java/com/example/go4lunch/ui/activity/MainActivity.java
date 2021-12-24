9cf17d70ed8c13c660447b26784237822a3dc0df
KS <seddikkamel1@gmail.com>
2021-12-24 11:55:27 +0100

    Creation of the clean architecture

--- app/src/main/java/com/example/go4lunch/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/go4lunch/ui/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.go4lunch;
+package com.example.go4lunch.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.go4lunch.R;
 
 public class MainActivity extends AppCompatActivity {
 
